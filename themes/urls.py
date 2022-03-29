6b03fe3654b347ce8634ff7e59bb32bc2e76f0c4
Abzal <64380003+Fistlol@users.noreply.github.com>
2022-03-30 01:06:54 +0600

    Update urls.py

--- themes/urls.py
+++ themes/urls.py
@@ -5,8 +5,9 @@ from . import views
 
 app_name = 'themes'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:pk>/', views.theme, name='theme'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    
+    path('', views.index, name='index'),
+    path('<int:pk>/', views.theme, name='theme'),
 ]
