58246df5ac1cc407fb8b4e1041088bd9202c7f01
Abzal <64380003+Fistlol@users.noreply.github.com>
2022-03-30 11:20:29 +0600

    Update views.py

--- themes/views.py
+++ themes/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import authenticate
-from django.contrib.auth import login as django_login
-from django.contrib.auth import logout as django_logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from .models import Theme
 
 
-def login(request):
+def loginPage(request):
     if request.user.is_authenticated:
         return redirect('index')
     else:
@@ -20,7 +18,7 @@ def login(request):
             user = authenticate(request, username=username, password=password)
 
             if user is not None:
-                django_login(request, user)
+                login(request, user)
                 return redirect('index')
             else:
                 messages.info(request, 'Логин или пароль введен неверно')
@@ -28,8 +26,8 @@ def login(request):
         return render(request, 'themes/login.html', {})
 
 
-def logout(request):
-    django_logout(request)
+def logoutUser(request):
+    logout(request)
     return redirect('login')
 
 
