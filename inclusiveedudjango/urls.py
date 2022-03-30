19336887f26423645b5048eff46205fca652a629
Abzal <64380003+Fistlol@users.noreply.github.com>
2022-03-30 18:42:19 +0600

    Update urls.py

--- inclusiveedudjango/urls.py
+++ inclusiveedudjango/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
     path('', include('themes.urls')),
     path('admin/', admin.site.urls),
 ]
